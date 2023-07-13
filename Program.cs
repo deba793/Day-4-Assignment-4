961078a3af094b80447b5764ac522eea28c2f772
deba793 <debasisrout793@gmail.com>
2023-07-13 14:32:34 +0530

    Assignment-4

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Check if the number is positive, negative, or zero
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+
+            // Check if the number is even or odd using the conditional operator
+            string parity = (number % 2 == 0) ? "even" : "odd";
+            Console.WriteLine("The number is " + parity + ".");
+        }
+    
+    }
+}
